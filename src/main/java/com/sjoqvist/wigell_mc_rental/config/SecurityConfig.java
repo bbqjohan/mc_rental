d75779cc71c3e585b869f10682005d9750dc98af
Markus Sjöqvist <korvkod@gmail.com>
2026-04-13 15:23:41 +0200

    gateway and dashboard support

--- src/main/java/com/sjoqvist/wigell_mc_rental/config/SecurityConfig.java
+++ src/main/java/com/sjoqvist/wigell_mc_rental/config/SecurityConfig.java
@@ -30,8 +30,7 @@ public class SecurityConfig {
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
                         auth ->
-                                auth.requestMatchers("/actuator/**")
-                                        .hasRole("ADMIN")
+                                auth.requestMatchers("/actuator/**").permitAll()
                                         .requestMatchers("/api/v1/customers/**")
                                         .hasRole("ADMIN")
                                         .requestMatchers("/api/v1/bikes")
